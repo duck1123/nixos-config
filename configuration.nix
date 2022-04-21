5e87b2a80f5b48519f4ecd48ffa8761452333478
Duck Nebuchadnezzar <duck@kronkltd.net>
2022-04-21 00:54:51 +0000

    updates

--- configuration.nix
+++ configuration.nix
@@ -39,7 +39,7 @@ in {
   # Allow unfree packages (e.g. Spotify)
   nixpkgs.config.allowUnfree = true;
 
-  # networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "nixos-vm"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
@@ -50,9 +50,9 @@ in {
   console = {
     font = "ter-i16b";
     packages = with pkgs; [ terminus_font ];
-    #   consoleFont = "Lat2-Terminus16";
-    #   consoleKeyMap = "us";
-    #   defaultLocale = "en_US.UTF-8";
+    # consoleFont = "Lat2-Terminus16";
+    # consoleKeyMap = "us";
+    # defaultLocale = "en_US.UTF-8";
   };
 
   # Set your time zone.
@@ -75,7 +75,6 @@ in {
     less
     xclip
     firefox
-    spotify
     zip
     unzip
     binutils
@@ -86,6 +85,7 @@ in {
     telnet
     dmenu
   ];
+
   programs.zsh.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
@@ -96,10 +96,10 @@ in {
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
+  # services.openssh.enable = true;
 
-  services.postgresql.enable = true;
-  services.postgresql.package = pkgs.postgresql_12;
+  # services.postgresql.enable = true;
+  # services.postgresql.package = pkgs.postgresql_12;
 
   # Enable mongodb
   # services.mongodb.enable = true;
@@ -121,53 +121,14 @@ in {
   services.xserver = {
     enable = true;
     layout = "us";
-    xkbOptions = "eurosign:e";
-    desktopManager = {
-      xfce = {
-        enable = true;
-        noDesktop = false;
-        enableXfwm = true;
-      };
-    };
-    displayManager = {
-      defaultSession = "xfce";
-    };
-    windowManager.xmonad = {
-      enable = false;
-      enableContribAndExtras = true;
-      extraPackages = haskellPackages: with haskellPackages; [
-        haskellPackages.xmonad-contrib
-        haskellPackages.xmonad-extras
-        haskellPackages.xmonad
-      ];
-    };
-    displayManager.lightdm.enable = true;
-    displayManager.sessionCommands = ''
-            xrdb "${pkgs.writeText  "xrdb.conf" ''
-            URxvt.font:                 xft:Dejavu Sans Mono for Powerline:size=14
-            XTerm*faceName:             xft:Dejavu Sans Mono for Powerline:size=14
-            XTerm*utf8:                 2
-            URxvt.letterSpace:          0
-            URxvt*saveLines:            32767
-            XTerm*saveLines:            32767
-            URxvt.url-select.launcher:  /usr/bin/firefox -new-tab
-            URxvt.url-select.underline: true
-            Xft*dpi:                    96
-            Xft*antialias:              true
-            Xft*hinting:                full
-            URxvt.scrollBar:            false
-            URxvt*scrollTtyKeypress:    true
-            URxvt*scrollTtyOutput:      false
-            URxvt*scrollWithBuffer:     false
-            URxvt*scrollstyle:          plain
-            URxvt*secondaryScroll:      true
-            Xft.autohint: 0
-            Xft.lcdfilter:  lcddefault
-            Xft.hintstyle:  hintfull
-            Xft.hinting: 1
-            Xft.antialias: 1
-            ''}"
-            '';
+    displayManager.gdm.enable = true;
+    displayManager.gdm.wayland = false;
+    desktopManager.gnome3.enable = true;
+  };
+
+  services = {
+    dbus.packages = [ pkgs.gnome3.dconf ];
+    udev.packages = [ pkgs.gnome3.gnome-settings-daemon ];
   };
 
   # services.xserver.layout = "us";
