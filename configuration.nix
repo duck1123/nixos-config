f7fb72ce74e72082d96e200bc58fbbc578eb2c42
Duck Nebuchadnezzar <duck@kronkltd.net>
2022-06-18 13:16:48 +0000

    reformat

--- configuration.nix
+++ configuration.nix
@@ -5,145 +5,143 @@
 { config, lib, pkgs, ... }:
 
 let
-moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
-nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
-my_python_packages = python-packages: with python-packages; [
+  moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
+  nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
+  my_python_packages = python-packages: with python-packages; [
     pynvim
     nvr
-];
-python_with_my_packages = nixpkgs.python3.withPackages my_python_packages;
-in
+  ];
+  python_with_my_packages = nixpkgs.python3.withPackages my_python_packages;
+in {
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    ./nvim.nix
+    ./zsh.nix
+    # ./base16/default.nix
+  ];
 
-{
-    imports =
-        [ # Include the results of the hardware scan.
-        ./hardware-configuration.nix
-            ./nvim.nix
-            ./zsh.nix
-# ./base16/default.nix
-        ];
+  # Use the GRUB 2 boot loader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  # boot.loader.grub.efiSupport = true;
+  # boot.loader.grub.efiInstallAsRemovable = true;
+  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  # Define on which hard drive you want to install Grub.
+  boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
 
-# Use the GRUB 2 boot loader.
-    boot.loader.grub.enable = true;
-    boot.loader.grub.version = 2;
-# boot.loader.grub.efiSupport = true;
-# boot.loader.grub.efiInstallAsRemovable = true;
-# boot.loader.efi.efiSysMountPoint = "/boot/efi";
-# Define on which hard drive you want to install Grub.
-    boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
-
-        systemd.coredump.enable = true;
-    security.pam.loginLimits = [
+  systemd.coredump.enable = true;
+  security.pam.loginLimits = [
     { domain = "*"; item = "core"; type = "soft"; value = "unlimited"; }
-    ];
-    systemd.extraConfig = "DefaultLimitCORE=1000000";
+  ];
+  systemd.extraConfig = "DefaultLimitCORE=1000000";
 
-# Allow unfree packages (e.g. Spotify)
-    nixpkgs.config.allowUnfree = true;
+  # Allow unfree packages (e.g. Spotify)
+  nixpkgs.config.allowUnfree = true;
 
-# networking.hostName = "nixos"; # Define your hostname.
-# networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  # networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-# Configure network proxy if necessary
-# networking.proxy.default = "http://user:password@proxy:port/";
-# networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-# Select internationalisation properties.
-    console = {
-        font = "ter-i16b";
-        packages = with pkgs; [ terminus_font ];
-#   consoleFont = "Lat2-Terminus16";
-#   consoleKeyMap = "us";
-#   defaultLocale = "en_US.UTF-8";
-    };
+  # Select internationalisation properties.
+  console = {
+    font = "ter-i16b";
+    packages = with pkgs; [ terminus_font ];
+    #   consoleFont = "Lat2-Terminus16";
+    #   consoleKeyMap = "us";
+    #   defaultLocale = "en_US.UTF-8";
+  };
 
-# Set your time zone.
-# time.timeZone = "Europe/Amsterdam";
+  # Set your time zone.
+  # time.timeZone = "Europe/Amsterdam";
 
-    nix.nixPath = [
-        "nixos-config=/etc/nixos/configuration.nix"
-            "nixpkgs=/etc/nixos/nixpkgs-channels"
-            "nixpkgs-overlays=/etc/nixos/overlays"
-    ];
+  nix.nixPath = [
+    "nixos-config=/etc/nixos/configuration.nix"
+    "nixpkgs=/etc/nixos/nixpkgs-channels"
+    "nixpkgs-overlays=/etc/nixos/overlays"
+  ];
 
 
-# List packages installed in system profile. To search, run:
-# $ nix search wget
-    environment.systemPackages = with pkgs; [
-        wget
-            vim
-            myNeovim
-            less
-            xclip
-            firefox
-            spotify
-            zip
-            unzip
-            binutils
-            gcc
-            gdb
-            nodejs
-            python_with_my_packages
-            telnet
-            dmenu
-    ];
-    programs.zsh.enable = true;
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    wget
+    vim
+    myNeovim
+    less
+    xclip
+    firefox
+    spotify
+    zip
+    unzip
+    binutils
+    gcc
+    gdb
+    nodejs
+    python_with_my_packages
+    telnet
+    dmenu
+  ];
+  programs.zsh.enable = true;
 
-# Some programs need SUID wrappers, can be configured further or are
-# started in user sessions.
-# programs.mtr.enable = true;
-# programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
 
-# List services that you want to enable:
+  # List services that you want to enable:
 
-# Enable the OpenSSH daemon.
-    services.openssh.enable = true;
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
 
-    services.postgresql.enable = true;
-    services.postgresql.package = pkgs.postgresql_12;
+  services.postgresql.enable = true;
+  services.postgresql.package = pkgs.postgresql_12;
 
-# Enable mongodb
-# services.mongodb.enable = true;
+  # Enable mongodb
+  # services.mongodb.enable = true;
 
-# Open ports in the firewall.
-# networking.firewall.allowedTCPPorts = [ ... ];
-# networking.firewall.allowedUDPPorts = [ ... ];
-# Or disable the firewall altogether.
-# networking.firewall.enable = false;
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
 
-# Enable CUPS to print documents.
-# services.printing.enable = true;
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
 
-# Enable sound.
-    sound.enable = true;
-    hardware.pulseaudio.enable = true;
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
 
-# Enable the X11 windowing system.
-    services.xserver = {
+  # Enable the X11 windowing system.
+  services.xserver = {
+    enable = true;
+    layout = "us";
+    xkbOptions = "eurosign:e";
+    desktopManager = {
+      xfce = {
         enable = true;
-        layout = "us";
-        xkbOptions = "eurosign:e";
-        desktopManager = {
-            xfce = {
-                enable = true;
-                noDesktop = true;
-                enableXfwm = false;
-            };
-        };
-        displayManager = {
-            defaultSession = "xfce+xmonad";
-        };
-        windowManager.xmonad = {
-            enable = true;
-            enableContribAndExtras = true;
-            extraPackages = haskellPackages: with haskellPackages; [
-                haskellPackages.xmonad-contrib
-                    haskellPackages.xmonad-extras
-                    haskellPackages.xmonad
-            ];
-        };
-        displayManager.lightdm.enable = true;
-        displayManager.sessionCommands = ''
+        noDesktop = true;
+        enableXfwm = false;
+      };
+    };
+    displayManager = {
+      defaultSession = "xfce+xmonad";
+    };
+    windowManager.xmonad = {
+      enable = true;
+      enableContribAndExtras = true;
+      extraPackages = haskellPackages: with haskellPackages; [
+        haskellPackages.xmonad-contrib
+        haskellPackages.xmonad-extras
+        haskellPackages.xmonad
+      ];
+    };
+    displayManager.lightdm.enable = true;
+    displayManager.sessionCommands = ''
             xrdb "${pkgs.writeText  "xrdb.conf" ''
             URxvt.font:                 xft:Dejavu Sans Mono for Powerline:size=14
             XTerm*faceName:             xft:Dejavu Sans Mono for Powerline:size=14
@@ -169,29 +167,29 @@ in
             Xft.antialias: 1
             ''}"
             '';
-    };
+  };
 
-# services.xserver.layout = "us";
-# services.xserver.xkbOptions = "eurosign:e";
+  # services.xserver.layout = "us";
+  # services.xserver.xkbOptions = "eurosign:e";
 
-# Enable touchpad support.
-# services.xserver.libinput.enable = true;
+  # Enable touchpad support.
+  # services.xserver.libinput.enable = true;
 
-# Enable the KDE Desktop Environment.
-# services.xserver.displayManager.sddm.enable = true;
-# services.xserver.desktopManager.plasma5.enable = true;
+  # Enable the KDE Desktop Environment.
+  # services.xserver.displayManager.sddm.enable = true;
+  # services.xserver.desktopManager.plasma5.enable = true;
 
-# Define a user account. Don't forget to set a password with ‘passwd’.
-    users.users.blake = {
-        isNormalUser = true;
-        extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-            shell = pkgs.zsh;
-    };
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.blake = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    shell = pkgs.zsh;
+  };
 
-# This value determines the NixOS release with which your system is to be
-# compatible, in order to avoid breaking some software such as database
-# servers. You should change this only after NixOS release notes say you
-# should.
-    system.stateVersion = "19.03"; # Did you read the comment?
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "19.03"; # Did you read the comment?
 
 }
